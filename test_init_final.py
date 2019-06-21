2c9a042caa83449b2e9aded3140f9bad1af92a67
showgun3866 <52000164+showgun3866@users.noreply.github.com>
2019-06-21 22:23:10 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1135,58 +1135,6 @@ async def on_message(msg):
 
 		##################################
 		
-	if message.content.startswith("!롤"):
-        learn = message.content.split(" ")
-        location = learn[1]
-        enc_location = urllib.parse.quote(location)
-
-        url = "http://www.op.gg/summoner/userName=" + enc_location
-        html = urllib.request.urlopen(url)
-
-        bsObj = bs4.BeautifulSoup(html, "html.parser")
-        rank1 = bsObj.find("div", {"class": "TierRankInfo"})
-        rank2 = rank1.find("div", {"class": "TierRank"})
-        rank3 = rank2.find("span", {"class": "tierRank"})
-        rank4 = rank3.text  # 티어표시 (브론즈1,2,3,4,5 등등)
-        print(rank4)
-        if rank4 != 'Unranked':
-          jumsu1 = rank1.find("div", {"class": "TierInfo"})
-          jumsu2 = jumsu1.find("span", {"class": "LeaguePoints"})
-          jumsu3 = jumsu2.text
-          jumsu4 = jumsu3.strip()#점수표시 (11LP등등)
-          print(jumsu4)
-
-          winlose1 = jumsu1.find("span", {"class": "WinLose"})
-          winlose2 = winlose1.find("span", {"class": "wins"})
-          winlose2_1 = winlose1.find("span", {"class": "losses"})
-          winlose2_2 = winlose1.find("span", {"class": "winratio"})
-
-          winlose2txt = winlose2.text
-          winlose2_1txt = winlose2_1.text
-          winlose2_2txt = winlose2_2.text #승,패,승률 나타냄  200W 150L Win Ratio 55% 등등
-
-          print(winlose2txt + " " + winlose2_1txt + " " + winlose2_2txt)
-
-        channel = message.channel
-        embed = discord.Embed(
-            title='롤 정보',
-            description='롤 정보입니다.',
-            colour=discord.Colour.green()
-        )
-        if rank4=='Unranked':
-            embed.add_field(name='당신의 티어', value=rank4, inline=False)
-            embed.add_field(name='-당신은 언랭-', value="언랭은 더이상의 정보는 제공하지 않습니다.", inline=False)
-            await client.send_message(channel, embed=embed)
-        else:
-         embed.add_field(name='당신의 티어', value=rank4, inline=False)
-         embed.add_field(name='당신의 LP(점수)', value=jumsu4, inline=False)
-         embed.add_field(name='당신의 승,패 정보', value=winlose2txt+" "+winlose2_1txt, inline=False)
-         embed.add_field(name='당신의 승률', value=winlose2_2txt, inline=False)
-         await client.send_message(channel, embed=embed)
-		
-		##################################
-		
-
 		if message.content.startswith('!현재시간'):
 			now3 = datetime.datetime.now() + datetime.timedelta(hours = int(basicSetting[0]))
 			await client.send_message(client.get_channel(channel), now3.strftime('%Y-%m-%d') + '   ' + now3.strftime('%H:%M:%S'), tts=False)
